b4fd32f633e06ac5fd624a8156edbaaf732b9426
Gerson <occoner24023@gmail.com>
2025-04-01 17:15:56 -0500

    modificando build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-set -o errexit
+#!/bin/bash
+set -o errexit  # Hace que el script falle si hay algún error.
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Instalar dependencias
 pip install -r requirements.txt
 
-# Convert static asset files
+# Recopilar archivos estáticos
 python manage.py collectstatic --no-input
 
-
+# Ejecutar migraciones de base de datos
 python manage.py migrate
-
