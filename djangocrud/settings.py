31943747b092feec46bcd59991cd7f98cbb7ae62
Gerson <occoner24023@gmail.com>
2025-04-02 10:30:48 -0500

    configuracion base de datos

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/djangocrud',
         conn_max_age=600    )}
 
 
