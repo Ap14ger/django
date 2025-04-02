fe95a523a3cc19ef10ac4692a7ca6f40e6d2517a
Gerson <occoner24023@gmail.com>
2025-04-02 07:38:04 -0500

    modificacion base de datos

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -85,12 +85,9 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 
 DATABASES = {
-        'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"), 
-        conn_max_age=600
-    )
-        
-}
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600    )}
 
 
 # Password validation
